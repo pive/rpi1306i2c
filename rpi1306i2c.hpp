4414ae6a407ab82dd0a59bfa521a8e91d3e68145
pive <pveber@gmail.com>
2023-08-02 15:53:03 +0100

    WIP: rework to allow use with TCA9548A

--- rpi1306i2c.hpp
+++ rpi1306i2c.hpp
@@ -7,12 +7,31 @@
 #include <string>
 #include <stdexcept>
 #include <iostream>
+#include <mutex>
 
 // system headers
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
+
+// debug prints
+#include <stdio.h>
+#if 1
+#include <iostream>
+
+namespace debug
+{
+    template<typename... Args>
+    void print(const char* file, int line, Args... args) {
+      fprintf(stderr, std::forward<Args>(args)...);
+    }
+}
+
+#define DEBUG(...)  debug::print(__FILE__, __LINE__, __VA_ARGS__)
+#else
+#define DEBUG(...)  ((void)0)
+#endif
 
 #define HZ_ADDR_MODE        0x00
 #define VT_ADDR_MODE        0x01
@@ -105,28 +124,58 @@ constexpr uint8_t ssd1306_128x64_init_seq[] = {
 
 namespace i2c {
 
+  template <uint8_t devid>
   class Device {
     private:
+      std::string m_i2c_dev_path;
+      int m_dev = -1;
+      std::mutex& m_mutex;
+
       uint8_t m_buffer[128];
       uint8_t m_dataSize = 0;
-      int m_dev = -1;
+
+      Device(std::mutex& mutex): m_mutex(mutex) {
+        m_i2c_dev_path = "/dev/i2c-" + std::to_string(devid);
+        m_dev = open(m_i2c_dev_path.c_str(), O_RDWR);
+        if (m_dev < 0) {
+          throw std::runtime_error(std::string("Could not open ") + m_i2c_dev_path);
+        }
+        DEBUG("Opened i2c device %s.\n", m_i2c_dev_path.c_str());
+      }
 
     public:
 
-      Device(uint8_t dev, uint8_t addr) {
-        std::string devPath = "/dev/i2c-";
-        devPath += std::to_string(dev);
-
-        m_dev = open(devPath.c_str(), O_RDWR);
-        if (m_dev < 0) {
-          throw std::runtime_error(std::string("Could not open ") + devPath);
-        }
-        if (ioctl(m_dev, I2C_SLAVE, addr) < 0) {
-          throw std::runtime_error(std::string("Could not open ") + std::to_string(addr));
+      virtual ~Device() {
+        if (m_dev >= 0) {
+          close(m_dev);
+          m_dev = -1;
+          DEBUG("Closed i2c device %s.\n", m_i2c_dev_path.c_str());
         }
       }
 
+      static const Device<devid>& get() {
+        static std::mutex s_mutex;
+        static const Device<devid> device(s_mutex);
+        return device;
+      }
+
+      void i2cOpen(uint8_t address) {
+        // HERE: check dev is not already in use (i2cOpen)
+        m_mutex.lock();
+        if (ioctl(m_dev, I2C_SLAVE, address) < 0) {
+          DEBUG("Could not open i2c address 0x%x.\n", address);
+          throw std::runtime_error(std::string("Could not open ") + std::to_string(address));
+        }
+        DEBUG("Opened i2c address 0x%x.\n", address);
+      }
+
+      void i2cClose() {
+        // HERE: make dev not in use
+        m_mutex.unlock();
+      }
+
       void bufferReset() {
+        // NOTE: might also need to protect the buffer
         m_dataSize = 0;
       }
 
@@ -149,97 +198,121 @@ namespace i2c {
           throw std::runtime_error("Could not write on device");
         }
       }
-
-      ~Device() {
-        if (m_dev >= 0) {
-          close(m_dev);
-          m_dev = -1;
-        }
-      }
   };
+
 }
 
 namespace ssd1306 {
 
   using Bitmap = std::span<const uint8_t>;
+  enum Height {
+    HEIGHT_32,
+    HEIGHT_64,
+  };
 
-  class Display: i2c::Device {
+  template <uint8_t devid>
+  class Display {
     private:
+  
+      uint8_t m_i2c_address;
+
       uint8_t m_width = 128;
-
-    protected:
-      using i2c::Device::bufferWrite;
-
       uint8_t m_height = 0;
 
-      Display(uint8_t dev, uint8_t addr): i2c::Device(dev, addr) {};
-
       void initDisplay(const uint8_t* sequence, uint8_t size) {
-        bufferReset();
+        auto i2c_device = i2c::Device<devid>::get();
+        i2c_device.i2cOpen(m_i2c_address);
+
+        i2c_device.bufferReset();
         uint8_t command[2] = { 0x00, 0x00 };
         for (uint8_t i = 0; i < size; i++) {
           command[1] = sequence[i];
-          directWrite(&command[0], sizeof(command));
+          i2c_device.directWrite(&command[0], sizeof(command));
         }
+
+        i2c_device.i2cClose();
       }
 
       void setBlock(uint8_t x, uint8_t y, uint8_t w) {
-        bufferReset();
-        bufferWrite(0x00);
-        bufferWrite(COLUMNADDR);
-        bufferWrite(x);
-        bufferWrite(w ? (x + w - 1) : (m_width - 1));
-        bufferWrite(PAGEADDR);
-        bufferWrite(y);
-        bufferWrite((m_height >> 3) - 1);
-        bufferFlush();
-        bufferWrite(0x40);      
+        auto i2c_device = i2c::Device<devid>::get();
+        i2c_device.i2cOpen(m_i2c_address);
+
+        i2c_device.bufferReset();
+        i2c_device.bufferWrite(0x00);
+        i2c_device.bufferWrite(COLUMNADDR);
+        i2c_device.bufferWrite(x);
+        i2c_device.bufferWrite(w ? (x + w - 1) : (m_width - 1));
+        i2c_device.bufferWrite(PAGEADDR);
+        i2c_device.bufferWrite(y);
+        i2c_device.bufferWrite((m_height >> 3) - 1);
+        i2c_device.bufferFlush();
+        i2c_device.bufferWrite(0x40);
+
+        i2c_device.i2cClose();
       }
 
-      void bufferWrite(const Bitmap& bitmap) {
+      void writeBitmap(const Bitmap& bitmap) {
+        auto i2c_device = i2c::Device<devid>::get();
+        i2c_device.i2cOpen(m_i2c_address);
+
         for (const auto& u: bitmap) {
-          bufferWrite(u);
+          i2c_device.bufferWrite(u);
         }
+
+        i2c_device.i2cClose();
       }
 
     public:
 
+      Display(uint8_t addr, Height height): m_i2c_address(addr) {
+        init(height);
+      };
+
+      void init(Height height) {
+        switch (height) {
+          case HEIGHT_32:
+            m_height = 32;
+            initDisplay(ssd1306_128x32_init_seq, sizeof(ssd1306_128x32_init_seq));
+            break;
+          case HEIGHT_64:
+            m_height = 64;
+            initDisplay(ssd1306_128x64_init_seq, sizeof(ssd1306_128x64_init_seq));
+            break;
+          default:
+            throw std::runtime_error("Nope.");
+        }
+      }
+
       void draw(uint8_t x, uint8_t y, uint8_t w, uint8_t h, const Bitmap& bitmap) {
         uint8_t j;
+        auto i2c_device = i2c::Device<devid>::get();
+        i2c_device.i2cOpen(m_i2c_address);
+
         setBlock(x, y >> 3, w);
-        bufferWrite(bitmap);
-        bufferFlush();  
+        writeBitmap(bitmap);
+        i2c_device.bufferFlush();
+
+        i2c_device.i2cClose();
       }
 
       void clear(uint8_t x, uint8_t y, uint8_t w, uint8_t h) {
+        auto i2c_device = i2c::Device<devid>::get();
+        i2c_device.i2cOpen(m_i2c_address);
+
         setBlock(x, y >> 3, w);
         // no need to iterate, write all the fucking way
         for (uint8_t m = (h >> 3); m > 0; m--) {
           for (uint8_t n = w; n > 0; n--) {
-            bufferWrite(0x00);
+            i2c_device.bufferWrite(0x00);
           }
         }
-        bufferFlush();
+        i2c_device.bufferFlush();
+
+        i2c_device.i2cClose();
       }
 
       void clear() {
         clear(0, 0, m_width, m_height);
-      }
-  };
-
-  class Display128x32: public Display {
-    public:
-      Display128x32(uint8_t dev, uint8_t addr): Display(dev, addr) {
-        m_height = 32;
-        initDisplay(ssd1306_128x32_init_seq, sizeof(ssd1306_128x32_init_seq));
-      }
-  };
-
-  class Display128x64: public Display {
-    public:
-      Display128x64(uint8_t dev, uint8_t addr): Display(dev, addr) {
-        m_height = 64;
-        initDisplay(ssd1306_128x64_init_seq, sizeof(ssd1306_128x64_init_seq));
       }
   };
 }
--- tca9548a.cpp
+++ tca9548a.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h>
 
 #include <span>
+#include <map>
+#include <mutex>
 
 #include "rpi1306i2c.hpp"
 
@@ -18,8 +20,38 @@ constexpr std::span<const uint8_t> bitmap {
   sizeof(bitmap_data)
 };
 
+// class Tca9548a: public ssd1306::Display {
+//   private:
+//     std::map<uint8_t, ssd1306::Display&> m_displays;
+//     std::mutex m_display_lock;
+//   public:
+//     Tca9548a(uint8_t dev, uint8_t addr): Display(dev, addr) {};
+
+//     void addDisplay(uint8_t slot, ssd1306::Display& display) {
+//       m_displays.insert({ slot, display });
+//     }
+
+//     void releaseDisplay(uint8_t slot) {
+//      }
+// };
+
+template <uint8_t device, uint8_t addr>
+class Mux {
+  public:
+    void select(uint8_t channel) {
+      auto i2c_device = i2c::Device<device>::get();
+      i2c_device.i2cOpen(addr);
+      uint8_t buf = 1 << channel;
+      i2c_device.directWrite(&buf, sizeof(buf));
+      i2c_device.i2cClose();
+    };
+};
+
 int main(void) {
+  Mux mux = Mux<1, 0x70>();
+  mux.select(0);
   ssd1306::Display<1> screen(0x3C, ssd1306::HEIGHT_32);
+  screen.clear();
   screen.draw(0, 0, 19, 32, bitmap);
   screen.draw(22, 0, 19, 32, bitmap);
   sleep(3);
